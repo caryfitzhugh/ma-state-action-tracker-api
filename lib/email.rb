400cbbc5f025aea4f8e3da9773d9b1cae5131e3a
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-12-11 22:36:33 -0500

    fix casting

--- lib/email.rb
+++ lib/email.rb
@@ -3,7 +3,7 @@ require 'aws-sdk'
 HOST = 'nescaum-ccsc-dataservices.com'
 
 def send_changed_email(action_track, user, action)
-  body = "Action Track (" + action_track.id + ") - " + action_track.title
+  body = "Action Track (" + action_track.id.to_s + ") - " + action_track.title
   # Replace sender@example.com with your "From" address.
   # This address must be verified with Amazon SES.
   sender = "alert@" + HOST
